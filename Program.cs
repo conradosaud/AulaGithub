6ff998ac9e6f31378821fe09d16bf44bbf7c8001
Conrado Saud <50385963+conradosaud@users.noreply.github.com>
2023-06-07 14:24:01 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaGithubSegQua
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá! Este é o meu projeto");
+            Console.WriteLine("Ele é um teste para o GitHub");
+            Console.WriteLine("Nada de novo por aqui...");
+            Console.ReadKey();
+        }
+    }
+}
